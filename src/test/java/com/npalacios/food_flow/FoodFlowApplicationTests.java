1050adf5eee1c57d8c2e6b35e259fa32e7ca9092
npalacios <nrodrigo.palaciov@gmail.com>
2026-03-13 16:01:54 -0600

    first commit

---
+++ src/test/java/com/npalacios/food_flow/FoodFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.npalacios.food_flow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
